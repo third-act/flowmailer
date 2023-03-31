f37145a943572a55472eb68f9b2ab9370a81171b
edwin.ljung <edwin.ljung@gmail.com>
2023-03-31 16:26:42 +0200

    added example file.

---
+++ examples/example.rs
@@ -0,0 +1,27 @@
+use flowmailer::{auth, Client, MailAddress, MailBuilder};
+
+#[tokio::main]
+async fn main() {
+    static CLIENT_ID: &str = "[ID]";
+    static CLIENT_SECRET: &str = "[SECRET]";
+    static ACCOUNT_ID: &str = "[ACCOUNT ID]";
+    let auth = auth::Auth::new(
+        auth::ClientSecret::new(CLIENT_SECRET.to_string()),
+        auth::ClientId::new(CLIENT_ID.to_string()),
+        auth::AccountId::new(ACCOUNT_ID.to_string()),
+    )
+    .unwrap();
+    let flowmailer = Client::new(auth);
+    let mailbuilder = MailBuilder::new_template(
+        MailAddress::new("sender.mail"),
+        MailAddress::new("receiver.mail"),
+        "[FLOW SELECTOR]",
+    )
+    // Set variables within the template.
+    .set_data(serde_json::json!({}))
+    .expect("set data")
+    // Set the subject of the mail.
+    .set_subject("test_flowmailer_template_mail");
+    // Sends the mail.
+    mailbuilder.send(&flowmailer).await.expect("");
+}
